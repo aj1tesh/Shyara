a130fb3748713196a2ff9a7b3852ec67fe47cdf7
Ajitesh <singh.ajitesh1449@gmail.com>
2025-06-24 23:01:36 +0530

    hash

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React, { useState } from 'react';
-import { BrowserRouter as Router, Routes, Route, NavLink } from 'react-router-dom';
+import { HashRouter as Router, Routes, Route, NavLink } from 'react-router-dom';
 import { Menu, X } from 'lucide-react';
 
 import HomePage from './pages/HomePage';
